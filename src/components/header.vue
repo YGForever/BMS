c6ff676ce96c3c2abbe9d4f181585ac6cc9869fd
unknown <1396522872@qq.com>
2019-12-03 18:11:28 +0800

    js

--- src/components/header.vue
+++ src/components/header.vue
@@ -13,15 +13,55 @@
           <li class="search"><a><i class="iconfont iconsousuo"></i></a></li>
           <li class="notice"><a><i class="iconfont icontongzhi"></i><span>12</span></a></li>
           <li class="email"><a><i class="iconfont iconemail" ></i><span>12</span></a></li>
-          <li class="usericon"><a><img src=""><span>用户名</span></a></li>
+          <!-- <li class="header-info class">
+            <div class="user" @click="get_user()">
+              <img src="@/assets/images/user.jpg">
+              <span>user</span>
+            </div>
+            <ul class="user_list" >
+                <li>修改密码</li>
+                <li>重置密码</li>
+                <li>绑定邮箱</li>
+            </ul>
+          </li> -->
+          <li class="header-info class">
+            <div class="user" @click="get_user()">
+              <img src="@/assets/images/user.jpg">
+              <span>user</span>
+            </div>
+            <div class="info-show">
+              <div @click="get_update()"><a href="">修改密码</a></div>
+              <div @click="get_rest()"><a href="">重置密码</a></div>
+              <div @click="get_bind()"><a href="">绑定邮箱</a></div>
+            </div>
+          </li>
+
+          <li class="loginout"><a><i class="iconfont iconloginOut" ></i></a></li>
         </ul>
       <!-- </div> -->
     </div>
 </template>
 <script>
 export default {
-    // components: {
-    //     header,
-    // }
+    mounted(){
+      console.log('用户信息')
+    },
+    data(){
+
+      return{
+        // show:false
+      }
+
+    },
+   methods:{
+    //  get_user(){
+       
+    //    this.show = true;
+    //    console.log(this.show)
+
+    //  }
+
+   }
+
 }
 </script>
